793ee51b529c16df060d7dd4630987dd8f714bce
Rekicho <up201604145@fe.up.pt>
2019-11-15 09:23:11 +0000

    Bug fix

--- src/BasicRiskPlayerAgent.java
+++ src/BasicRiskPlayerAgent.java
@@ -88,7 +88,7 @@ public class BasicRiskPlayerAgent extends Agent {
             while (it.hasNext()) {
                 Map.Entry pair = (Map.Entry) it.next();
                 Country actual = (Country) pair.getValue();
-                if (!actual.getOwner().equals(getAID()) && actual.getArmies() >= 2)
+                if (!actual.getOwner().equals(getAID()))
                     possibleCountriesToAttack.add(actual);
             }
         }
